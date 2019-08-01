19273c029ef4d399f5f4e6d808e4a8e8ed3a3599
Sid Sun <sid@sidsun.com>
2019-08-01 12:15:44 +0530

    Introduce the use of environment variables
    Signed-off-by: Sid Sun <sid@sidsun.com>

--- main.go
+++ main.go
@@ -6,12 +6,14 @@ import (
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 	_ "github.com/lib/pq"
 	"log"
+	"os"
+	"strconv"
 	"sync"
 )
 
 func main() {
 	var wg sync.WaitGroup
-	bot, err := tgbotapi.NewBotAPI("XXXXXXXXXXXXXXXXXXXXXX")
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("FREDA_API_TOKEN_ID"))
 	if err != nil {
 		log.Panic(err)
 	}
@@ -43,18 +45,16 @@ func getUpdates(bot *tgbotapi.BotAPI, updates tgbotapi.UpdatesChannel) {
 }
 
 func handleUpdate(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
-		if update.Message.From.ID == MESSAGEID  && update.Message.Chat.UserName == CHAT_USERNAME { //777000 is the ID of Telegram's replicating service for channel to discussion group.
-		 //REPLEACE CHAT_USERNAME above with the discussion Chat's public ID (or equivalent) to make sure it only replcates message from that group
-			fmt.Printf("message: %s\n", update.Message.Text)
+		if update.Message.From.ID == 777000  && update.Message.Chat.UserName == os.Getenv("TARGET_CHAT_USERNAME") { //777000 is the ID of Telegram's replicating service for channel to discussion group.
 			successful, addError := addToDatabase(update.Message.Text)
+			adminChatID, _ := strconv.ParseInt(os.Getenv("ADMIN_CHAT_ID"), 10, 64)
 			if successful {
-				msg := tgbotapi.NewMessage(YOUR_CHAT_ID, "") //Replace YOUR_CHAT_ID to get a message if bot is successful in adding it to the DB
-				msg.Text = "My Lord, I have added message successfully to database, I hope I am serving you well."
+				msg := tgbotapi.NewMessage(adminChatID, "My Lord, I have added message successfully to database, I hope I am serving you well.")
 				_, _ = bot.Send(msg)
 			} else { // Get a message is something goes south
-				msg := tgbotapi.NewMessage(YOUR_CHAT_ID, "Something failed, sending details. If you don't get the details in a message immediately after this one, It might be something very bad.")
+				msg := tgbotapi.NewMessage(adminChatID, "Something failed, sending details. If you don't get the details in a message immediately after this one, It might be something very bad.")
 				_, _ = bot.Send(msg) // addError.Error MAY lead to nil pointer derefernce which will cause a panic, I am not sure if that will ever happen in out case.
-				msg = tgbotapi.NewMessage(YOUR_CHAT_ID, "")
+				msg = tgbotapi.NewMessage(adminChatID, "")
 				msg.Text = "My Lord, I have failed in adding the message database, the error I encountered is:" + addError.Error() + "I am sorry to have disappointed you."
 				_, _ = bot.Send(msg)
 				// Do replace YOUR_CHAT_ID with your chat ID (or a group or anything, if you want that)
@@ -69,12 +69,12 @@ func handleUpdate(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
 }
 
 func addToDatabase(message string) (bool, error) {
-	const (
+	var (
 		host     = "localhost"
 		port     = 5432
-		user     = "GOOD_ADMIN" // Replace with required user
-		password = "MOST_SECURE_PASSWORD" // Replace with required passoword (if you use this pass, i'll kill you)
-		dbname   = "VERT_EFFICIENT_DB" // Replace with required DB name
+		user     = "postgres" // Replace with required user
+		password = os.Getenv("POSTGRES_PASSWORD")
+		dbname   = os.Getenv("FREDA_DB_NAME")
 	)
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
